836bf2275abc9769bf3e6ccd41c76a768858aaf6
Milind452 <mohapatra.milind@gmail.com>
2021-10-09 15:19:15 +0530

    Add project files.

---
+++ BankApp/Bank.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankApp
+{
+    class Bank
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
